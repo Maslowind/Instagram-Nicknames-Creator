857d96e86230350d91a25d4fcb2450c2e369fd6d
maslowind <bart1885@gmail.com>
2020-03-11 11:38:40 +0200

    Binary update

--- Services/nicknameHelper.js
+++ Services/nicknameHelper.js
@@ -1,18 +1,16 @@
   exports.getNicknameSymbols =function (email) {
     return email.substr(0, email.indexOf('@'))    
   };
-  
-
+ 
 
   exports.getNicknamesList =  function (nickname) {
     let nicknameList = [];
     arrTestNickname = getWorkArray(nickname);
-    for (let j = 0; j < Math.pow(2, nickname.length - 1); j++) {      
-      for (let i = 0; i < nickname.length - 1; i++) {
-        if (j % (Math.pow(2, i)) == 0) {        
-          if (arrTestNickname[2 * i + 1] === '') arrTestNickname[2 * i + 1] = '.';
-          else if (arrTestNickname[2 * i + 1] === '.') arrTestNickname[2 * i + 1] = '';
-        }      
+    for (let j = 0; j < Math.pow(2, nickname.length - 1); j++) { 
+      let dotBinary=j.toString(2);
+      for (let i = 0; i < dotBinary.length; i++) {
+        if (dotBinary[i] == 1) arrTestNickname[2 * i + 1] = '.';
+        else if (dotBinary[i] == 0) arrTestNickname[2 * i + 1] = '';
       }
        let another_one_nickname = arrTestNickname.join('');
        nicknameList.push (another_one_nickname);         
@@ -27,8 +25,6 @@
   
     for(let i = 0; i < arrNickname.length; i++) {
       arrTestNickname[2*i] = arrNickname[i];
-      arrTestNickname[2*i+1] = '';
-      
     }
     return arrTestNickname;   
   };
